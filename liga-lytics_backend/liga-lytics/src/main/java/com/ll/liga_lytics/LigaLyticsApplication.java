1ea80e9799e85c9d866ed5c8b7f7f521a7ee13f3
Abaan Noman <abaan1@yahoo.com>
2024-09-02 00:59:09 -0400

    Initial commit

---
+++ liga-lytics_backend/liga-lytics/src/main/java/com/ll/liga_lytics/LigaLyticsApplication.java
@@ -0,0 +1,13 @@
+package com.ll.liga_lytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaLyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaLyticsApplication.class, args);
+	}
+
+}
---
+++ liga-lytics_backend/liga-lytics/src/test/java/com/ll/liga_lytics/LigaLyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.liga_lytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaLyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
